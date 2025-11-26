bf7ddceaaefbdfaea07d1c653cfd98609dfb5239
Michal Madeiski <michal.madeiski@onet.pl>
2025-11-26 10:21:40 +0100

    Fix Lab07 - z1

--- Lab07/z1.py
+++ Lab07/z1.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
 import os
 import sys
 import time
 from threading import Timer
-from gpiozero import LED, Button
 from gpiozero import Device
 from gpiozero.pins.mock import MockFactory
-# from config import *
+# import RPi.GPIO as GPIO
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from config_mock import *
+# from config import *
 
 Device.pin_factory = MockFactory()
 
@@ -18,30 +18,22 @@ red_led = led1
 green_led = led2
 blue_led = led3
 
-green_button = buttonGreen
+red_button = buttonRed
 
-def light_rgb_on_button():
-    print("Naciśnij zielony przycisk")
-    
-    while True:
-        if green_button.is_pressed:
-            print("Zielony przycisk naciśnięty")
-        
-            red_led.on()
-            green_led.on()
-            blue_led.on()
-            time.sleep(1)
-            red_led.off()
-            green_led.off()
-            blue_led.off()
-
-def simulate_press():
-    print(">>>SYMULACJA: naciskanie zielonego przycisku")
-    green_button.pin.drive_low()
+def simulatePress():
+    print("\n>>>SYMULACJA: naciskanie zielonego przycisku")
+    red_button.pin.drive_low()
     time.sleep(0.1)
-    green_button.pin.drive_high()
+    red_button.pin.drive_high()
 
+def redButtonTest():
+    while not red_button.is_pressed:
+    # while GPIO.input(buttonRed) == GPIO.HIGH:
+        print('*', end='', flush=True)
+        time.sleep(0.1)
 
 if __name__ == "__main__":
-    Timer(2, simulate_press).start()
-    light_rgb_on_button()
+    print("Program started")
+    Timer(3, simulatePress).start()
+    redButtonTest()
+    print("Program finished")
